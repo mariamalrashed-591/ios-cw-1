613b1dff89ff64e4f2b3bbc77db12f6c136bc627
mariamalrashed-c57 <66645992+mariamalrashed-591@users.noreply.github.com>
2020-09-10 14:41:13 +0300

    cw 1.1 done

--- cw 1.1.playground/Contents.swift
+++ cw 1.1.playground/Contents.swift
@@ -7,3 +7,15 @@ var hobby1 = "baking" //string
 var hobby2 = "wakeboarding" //string
 
 print("Hello, my name is \(name) my height is \(height) my age is \(age) I enjoy \(hobby1) and \(hobby2) ")
+
+var friendName = "Noura" //string
+var friendHeight = 1.62 //double
+var friendAge = 19 //int
+var friendHobby1 = "baking" //string
+var friendHobby2 = "swimming" //string
+
+var ageDifference = friendAge - age
+print (ageDifference)
+
+var mutualHobby = (hobby1 == friendHobby1) || (hobby1 == friendHobby2) || ( hobby2 == friendHobby1) || (hobby2 == friendHobby2)
+print(mutualHobby)

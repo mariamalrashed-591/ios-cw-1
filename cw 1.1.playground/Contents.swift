923b84a7547c22b4eeef6d3f024946640efa6ed7
mariamalrashed-c57 <66645992+mariamalrashed-591@users.noreply.github.com>
2020-09-10 14:28:54 +0300

    cw 1.1 done

---
+++ cw 1.1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let name = "Mariam" //string
+var height = 1.60 //double
+var age = 18 //int
+var hobby1 = "baking" //string
+var hobby2 = "wakeboarding" //string
+
+print("Hello, my name is \(name) my height is \(height) my age is \(age) I enjoy \(hobby1) and \(hobby2) ")
